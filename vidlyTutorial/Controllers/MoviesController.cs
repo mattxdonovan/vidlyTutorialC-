ff1ee3a6a13886984eea540b3ca4beaa9b1f3552
mattxdonovan <mattxd12@gmail.com>
2017-02-28 10:30:37 -0700

    corrected error in Views folder

--- vidlyTutorial/Controllers/MoviesController.cs
+++ vidlyTutorial/Controllers/MoviesController.cs
@@ -4,25 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using vidlyTutorial.Models;
+using vidlyTutorial.ViewModels;
+
 
 namespace vidlyTutorial.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies/Random
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer() { Name = "Customer 1" },
+                new Customer() { Name = "Customer 2" },
 
-            return View(movie);
+            };
+
+            var viewModel = new RandomMovieViewModel();
+            {
+                viewModel.Movie = movie;
+                viewModel.Customers = customers;
+            };
+           /*
+            var viewResult = new ViewResult();
+            viewResult.ViewData.Model
+            */
+            return View(viewModel);
         }
 
         //Attribute routing with REGEX to limit length to 2 and range from 1-12
+        /*
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}"];
 
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
         }
+        */
            } 
     }
--- vidlyTutorial/Models/Customer.cs
+++ vidlyTutorial/Models/Customer.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace vidlyTutorial.Models
 {
-    public class Movie
+    public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
